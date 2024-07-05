c786dba49833ef1cec120a7fb2d2fe439da6c9ad
francotaind <francotaind00@gmail.com>
2024-07-05 15:24:47 +0300

    minor fixes

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,9 @@
+/***
+ * Returns an array of student ids.
+ * @param {Array} students - An array of student objects.
+ * @returns {Array} - An array of student ids.
+ */
+
 function getListStudentIds(students) {
   if (!Array.isArray(students)) {
     return [];
