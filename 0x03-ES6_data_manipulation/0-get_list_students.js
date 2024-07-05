7e760eeef27e174bbf9f4c468d65e5de161f4379
francotaind <francotaind00@gmail.com>
2024-07-05 09:57:01 +0300

    datamanip

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
+function getListStudents() {
+    return students;
+}
+export default getListStudents;
