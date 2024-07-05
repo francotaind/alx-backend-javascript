8ba5a532dfd6e006cdad862a4ea16f1ddd6f9d69
francotaind <francotaind00@gmail.com>
2024-07-05 15:26:15 +0300

    minor fixes

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,17 @@
-/***
- * Returns an array of student ids.
- * @param {Array} students - An array of student objects.
- * @returns {Array} - An array of student ids.
+/**
+ * Retrieves students in a given location.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {String} city - The location.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns
  */
-
-function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
-    return [];
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
   }
-  return students.map((student) => student.id);
+  return [];
 }
-export getLisStudentIds;
