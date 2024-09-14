c9cc2ca6c963de2e42cbe9f94278086fc1845116
francotaind <francotaind00@gmail.com>
2024-09-14 02:04:41 -0700

    minor syntax fixes

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,14 @@
 function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve("API response data");
-		}, 1000);
-	});
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = Math.random() > 0.5; // Simulate success/failure
+      if (success) {
+        resolve('API response data');
+      } else {
+        reject(new Error('API request failed'));
+      }
+    }, 1000);
+  });
 }
 
 export default getResponseFromAPI;
