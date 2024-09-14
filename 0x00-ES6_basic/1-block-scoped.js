d96f77590a5fd42fa8e2590963271bf268488480
francotaind <francotaind00@gmail.com>
2024-09-14 10:53:44 +0300

    deletions

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-var */
 export default function taskBlock(trueOrFalse) {
-    var task = false;
-    var task2 = true;
+    let task = false;
+    let task2 = true;
 
     if (trueOrFalse) {
-        const task = true;
-        const task2 = false;
+        let task = true;
+        let task2 = false;
     }
 
     return [task, task2];
