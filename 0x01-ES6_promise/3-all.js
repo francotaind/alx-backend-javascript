2711e84012643900de8c1c4b2ef767422db04008
francotaind <francotaind00@gmail.com>
2024-09-17 00:33:14 -0700

    README

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+function handleProfileSignup() {
+	return Promise.all([uploadPhoto(), createUser()])
+	.then(([photoResult, userResult]) => {
+	console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+	})
+	.catch(() => {
+		console.log('Signup system offline');
+	});
+}
+
+export default handleProfileSignup;
+
